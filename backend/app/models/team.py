4e93d87e3c45ea667855624311b890a91c0f9890
w.jamesdevera <w.jamesdevera@gmail.com>
2026-08-27 18:59:55 +0800

    feat: add team player relationship

--- backend/app/models/team.py
+++ backend/app/models/team.py
@@ -1,8 +1,17 @@
 from app.db.schema import Base
-from sqlalchemy.orm import mapped_column, Mapped
-from sqlalchemy import String, Integer, DateTime, func
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+from sqlalchemy import String, Integer, DateTime, func, Table, Column, ForeignKey
 from datetime import datetime
 from pydantic import BaseModel
+from typing import List
+from app.models.player import Player
+
+team_player_table = Table(
+    "team_player",
+    Base.metadata,
+    Column("team_id", ForeignKey("teams.id"), primary_key=True),
+    Column("player_id", ForeignKey("players.id"), primary_key=True),
+)
 
 
 class TeamORM(Base):
@@ -17,6 +26,10 @@ class TeamORM(Base):
     updated_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
     )
+
+    # Relationship
+    players: Mapped[List["Player"]] = relationship(
+        secondary=team_player_table, back_populates="players")
 
     def __repr__(self):
         return f"<Team(id={self.id}, name={self.name})>"
