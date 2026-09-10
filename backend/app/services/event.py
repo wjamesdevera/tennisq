65117c721fe106a2953f52d26beddea61f199887
w.jamesdevera <w.jamesdevera@gmail.com>
2026-09-10 22:31:56 +0800

    feat: add find_event service

--- backend/app/services/event.py
+++ backend/app/services/event.py
@@ -1,10 +1,12 @@
 from sqlalchemy import select
+from sqlalchemy.orm import joinedload
 from app.db.engine import AsyncSession
 from app.models.event import EventORM
 import uuid
 
 
-async def is_event_existing(event_id: uuid.UUID, session: AsyncSession) -> bool:
-    stmt = select(EventORM).where(EventORM.id == event_id)
+async def find_event(event_id: uuid.UUID, session: AsyncSession) -> EventORM:
+    stmt = select(EventORM).where(
+        EventORM.id == event_id).options(joinedload(EventORM.club))
     result = await session.execute(stmt)
     return result.scalar_one_or_none()
