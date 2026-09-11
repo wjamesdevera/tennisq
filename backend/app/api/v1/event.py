5bb0add4882a8d41925a1328d6b557fd2209d9ce
w.jamesdevera <w.jamesdevera@gmail.com>
2026-09-11 21:56:17 +0800

    feat: convert event service to EventService

--- backend/app/api/v1/event.py
+++ backend/app/api/v1/event.py
@@ -3,7 +3,7 @@ import uuid
 from app.services.players import find_player
 from fastapi import APIRouter, status, HTTPException
 from app.core.dependencies import AsyncSessionDep
-from app.services.event import find_event, add_player as event_service_add_player
+from app.services.event import EventService
 from pydantic import BaseModel
 
 router = APIRouter()
@@ -16,7 +16,8 @@ class PlayerIdModel(BaseModel):
 # PATCH /events/:event_id/players
 @router.patch('/{event_id}/players', status_code=status.HTTP_200_OK)
 async def add_player(event_id: uuid.UUID, player: PlayerIdModel, session: AsyncSessionDep):
-    event_obj = await find_event(event_id, session)
+    event_service = EventService(session)
+    event_obj = await event_service.find_event(event_id)
     if not event_obj:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
@@ -46,7 +47,7 @@ async def add_player(event_id: uuid.UUID, player: PlayerIdModel, session: AsyncS
             }
         )
 
-    updated_event = await event_service_add_player(player_obj.id, event_obj.id, session)
+    updated_event = await event_service.add_player(player_obj.id, event_obj.id, session)
 
     # if member is part of club add player to event
     return {"success": "ok", "result": updated_event}
--- backend/app/services/event.py
+++ backend/app/services/event.py
@@ -1,27 +1,30 @@
 from app.models.player import PlayerORM
 from sqlalchemy import select
 from sqlalchemy.orm import selectinload, joinedload
-from app.db.engine import AsyncSession
+from sqlalchemy.ext.asyncio import AsyncSession
 from app.models.event import EventORM
 import uuid
 
 
-async def find_event(event_id: uuid.UUID, session: AsyncSession) -> EventORM:
-    stmt = select(EventORM).where(
-        EventORM.id == event_id).options(joinedload(EventORM.club))
-    result = await session.execute(stmt)
-    return result.scalar_one_or_none()
+class EventService:
+    def __init__(self, session: AsyncSession):
+        self.session = session
 
+    async def find_event(self, event_id: uuid.UUID) -> EventORM:
+        stmt = select(EventORM).where(
+            EventORM.id == event_id).options(joinedload(EventORM.club))
+        result = await self.session.execute(stmt)
+        return result.scalar_one_or_none()
 
-async def add_player(player_id: uuid.UUID, event_id: uuid.UUID, session: AsyncSession) -> EventORM:
-    stmt = select(EventORM).where(
-        EventORM.id == event_id).options(selectinload(EventORM.players))
-    result = await session.execute(stmt)
-    event_obj = result.scalar_one_or_none()
-    stmt = select(PlayerORM).where(
-        PlayerORM.id == player_id)
-    result = await session.execute(stmt)
-    player_obj = result.scalar_one_or_none()
+    async def add_player(self, player_id: uuid.UUID, event_id: uuid.UUID) -> EventORM:
+        stmt = select(EventORM).where(
+            EventORM.id == event_id).options(selectinload(EventORM.players))
+        result = await self.session.execute(stmt)
+        event_obj = result.scalar_one_or_none()
+        stmt = select(PlayerORM).where(
+            PlayerORM.id == player_id)
+        result = await self.session.execute(stmt)
+        player_obj = result.scalar_one_or_none()
 
-    event_obj.players.append(player_obj)
-    return event_obj
+        event_obj.players.append(player_obj)
+        return event_obj
