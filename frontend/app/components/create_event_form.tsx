6bed97d30eb09c7b9e805fa286e181e6b04bfcda
w.jamesdevera <w.jamesdevera@gmail.com>
2026-09-21 17:30:09 +0800

    feat: add CreateEventForm component

--- frontend/app/components/create_event_form.tsx
+++ frontend/app/components/create_event_form.tsx
@@ -3,17 +3,27 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import * as z from "zod";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs from "dayjs";
 
 const CreateEventFormSchema = z.object({
   title: z.string().min(1, { message: "Title is required" }),
   description: z.string().optional(),
-  date: z.string().min(1, { message: "Date is required" }),
-  maxPlayers: z.coerce.number().int().positive().optional(),
+  date: z
+    .string()
+    .min(1, { message: "Date is required" })
+    .refine((value) => dayjs(value).isValid(), { message: "Date is required" })
+    .transform((value) => dayjs(value).format("YYYY-MM-DD")),
+  maxPlayers: z.coerce
+    .number()
+    .int()
+    .positive({ message: "Max players must be a positive integer" })
+    .gte(2, { message: "Max players must be greater than or equal to 2" })
+    .optional(),
 });
 
 type CreateEventFormInput = z.input<typeof CreateEventFormSchema>;
@@ -26,6 +36,7 @@ const CreateEventForm: React.FC = () => {
 
   const {
     register,
+    control,
     handleSubmit,
     formState: { errors },
   } = useForm<CreateEventFormInput, undefined, CreateEventFormOutput>({
@@ -33,7 +44,10 @@ const CreateEventForm: React.FC = () => {
   });
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col gap-4 p-2 max-w-md mx-auto"
+      >
         <TextField
           label="Title"
           {...register("title")}
@@ -47,7 +61,19 @@ const CreateEventForm: React.FC = () => {
           helperText={errors.description?.message}
         />
 
-        <DatePicker label="Date" />
+        <Controller
+          name="date"
+          control={control}
+          render={({ field }) => (
+            <DatePicker
+              label="Date"
+              value={field.value ? dayjs(field.value) : null}
+              onChange={(newValue) => {
+                field.onChange(newValue ? newValue.format("YYYY-MM-DD") : "");
+              }}
+            />
+          )}
+        />
         {errors?.date && <p>{errors.date.message}</p>}
 
         <TextField
