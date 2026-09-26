3d1d99630436968f56b6eb492f3e113287c6765f
Winfrey De Vera <w.jamesdevera@gmail.com>
2026-09-26 19:38:30 +0800

    feat: updated event schema
    separated proper event fields for different purpose

--- backend/app/schemas/event.py
+++ backend/app/schemas/event.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+import uuid
 
 from pydantic import BaseModel, ConfigDict, Field
 from datetime import datetime, date as date_type
@@ -9,20 +10,22 @@ if TYPE_CHECKING:
     from .player import Player
 
 
-class CreateEvent(BaseModel):
-    name: str
-    type: str
-    date: str = datetime.now().date()
-
-
 class Event(BaseModel):
-    model_config = ConfigDict(from_attributes=True)
-    id: str | None = None
     name: str
     description: str
     max_players: int
     date: date_type | None = datetime.now()
-    match_logs: List["MatchLog"] = Field(default_factory=list)
-    players: List["Player"] = Field(default_factory=list)
+
+    model_config = ConfigDict(from_attributes=True)
+    # match_logs: List["MatchLog"] = Field(default_factory=list)
+    # players: List["Player"] = Field(default_factory=list)
+
+
+class CreateEvent(Event):
+    pass
+
+
+class ReadEvent(Event):
+    id: uuid.UUID
     created_at: datetime | None = None
     updated_at: datetime | None = None
