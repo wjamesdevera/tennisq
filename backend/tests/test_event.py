26ba4bd5900c1632840e88037db0c2ad97628d6e
Winfrey De Vera <w.jamesdevera@gmail.com>
2026-09-26 20:40:20 +0800

    feat: added new test cases for event creation

--- backend/tests/test_event.py
+++ backend/tests/test_event.py
@@ -18,3 +18,89 @@ def test_create_event(client):
     assert data['event']['max_players'] == 30
     assert data['event']['created_at'] is not None
     assert data['event']['updated_at'] is not None
+
+
+def test_missing_name(client):
+    response = client.post(
+        "/v1/events", json={
+            "description": "Open Play Description",
+            "date": "2026-09-26",
+            "max_players": 30
+        }
+    )
+
+    assert response.status_code >= 400 and response.status_code < 500
+
+
+def test_missing_description(client):
+    response = client.post(
+        "/v1/events", json={
+            "name": "Open Play",
+            "date": "2026-09-26",
+            "max_players": 30
+        }
+    )
+
+    assert response.status_code >= 400 and response.status_code < 500
+
+
+def test_missing_date(client):
+    response = client.post(
+        "/v1/events", json={
+            "name": "Open Play",
+            "description": "Open Play Description",
+            "max_players": 30
+        }
+    )
+
+    assert response.status_code >= 400 and response.status_code < 500
+
+
+def test_missing_max_players(client):
+    response = client.post(
+        "/v1/events", json={
+            "name": "Open Play",
+            "description": "Open Play Description",
+            "date": "2026-09-26",
+        }
+    )
+
+    assert response.status_code >= 400 and response.status_code < 500
+
+
+def test_invalid_date_format(client):
+    response = client.post(
+        "/v1/events", json={
+            "name": "Open Play",
+            "description": "Open Play Description",
+            "date": "2026-09",
+        }
+    )
+
+    assert response.status_code >= 400 and response.status_code < 500
+
+
+def test_negative_max_players(client):
+    response = client.post(
+        "/v1/events", json={
+            "name": "Open Play",
+            "description": "Open Play Description",
+            "date": "2026-09-26",
+            "max_players": -10
+        }
+    )
+
+    assert response.status_code >= 400 and response.status_code < 500
+
+
+def test_above_max_players_limit(client):
+    response = client.post(
+        "/v1/events", json={
+            "name": "Open Play",
+            "description": "Open Play Description",
+            "date": "2026-09-26",
+            "max_players": 100
+        }
+    )
+
+    assert response.status_code >= 400 and response.status_code < 500
