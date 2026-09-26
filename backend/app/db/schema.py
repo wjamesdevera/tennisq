3512d2973135b8cbade567161072adec9ca86d2b
Winfrey De Vera <w.jamesdevera@gmail.com>
2026-09-26 20:23:49 +0800

    refactor: removed unused imports

--- backend/app/db/schema.py
+++ backend/app/db/schema.py
@@ -1,15 +1,5 @@
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import mapped_column, Mapped
-from sqlalchemy import func, DateTime
-from datetime import datetime, timezone
-from typing import Optional
-import uuid
 
 
 class Base(DeclarativeBase):
     pass
-
-# class EventTimestampModel(Base):
-#     occured_at: Mapped[datetime] = mapped_column(
-#         DateTime(timezone=True), server_default=func.now()
-#     )
